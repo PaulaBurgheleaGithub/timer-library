d533bdc8e71122e2925e4822e053a9d95aa0e6fa
Paula Burghelea <paulaburghelea.github@gmail.com>
2022-04-04 21:44:22 +0100

    corrected delta error and added eport to the first function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const min_factor = 60 * sec_factor;
 const hour_factor = 60 * min_factor;
 const day_factor = 24 * hour_factor;
 
-function timeExtract(totalTime, factor) {
+export function timeExtract(totalTime, factor) {
   const timeExtracted = Math.floor(totalTime / factor);
   const newTotalTime = totalTime - timeExtracted * factor;
   return [timeExtracted, newTotalTime];
@@ -18,7 +18,7 @@ export function getTimespan(dateA, dateB) {
 
   const delta = timestampB - timestampA; //positive difference
 
-  const [days, delta1] = timeExtract(delta1, day_factor);
+  const [days, delta1] = timeExtract(delta, day_factor);
   const [hours, delta2] = timeExtract(delta2, hour_factor);
   const [minutes, delta3] = timeExtract(delta3, min_factor);
   const [secounds] = timeExtract(delta3, sec_factor);
