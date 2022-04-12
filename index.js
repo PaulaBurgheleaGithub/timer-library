cdec3f916ec6c9a79a34b0a3c3045a90a5e65534
Paula Burghelea <paulaburghelea.github@gmail.com>
2022-04-12 11:03:21 +0100

    correct

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /* Our functionality for determining amount of time between two dates */
-const sec_factor = 1000;
-const min_factor = 60 * sec_factor;
-const hour_factor = 60 * min_factor;
-const day_factor = 24 * hour_factor;
+
+const SEC_FACTOR = 1000; // 1000ms / SEC_MATH = 1
+const MIN_FACTOR = 60 * SEC_FACTOR;
+const HOUR_FACTOR = 60 * MIN_FACTOR;
+const DAY_FACTOR = 24 * HOUR_FACTOR;
 
 export function timeExtract(totalTime, factor) {
   const timeExtracted = Math.floor(totalTime / factor);
@@ -13,15 +14,15 @@ export function timeExtract(totalTime, factor) {
 //asume dateB > dateA
 
 export function getTimespan(dateA, dateB) {
-  const timestampA = dateA.getTime();
-  const timestampB = dateB.getTime();
+  const timestampA = dateA.getTime(); // span between Jan 1, 1970 and dateA
+  const timestampB = dateB.getTime(); // span between Jan 1, 1970 and dateB
 
   const delta = timestampB - timestampA; //positive difference
 
-  const [days, delta1] = timeExtract(delta, day_factor);
-  const [hours, delta2] = timeExtract(delta2, hour_factor);
-  const [minutes, delta3] = timeExtract(delta3, min_factor);
-  const [secounds] = timeExtract(delta3, sec_factor);
+  const [days, delta1] = timeExtract(delta, DAY_FACTOR);
+  const [hours, delta2] = timeExtract(delta1, HOUR_FACTOR);
+  const [minutes, delta3] = timeExtract(delta2, MIN_FACTOR);
+  const [secounds] = timeExtract(delta3, SEC_FACTOR);
 
   return [days, hours, minutes, secounds];
 }
